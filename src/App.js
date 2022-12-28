ea4b6a38bdc0d1e2da842d250112a0af3934b95e
yasinarafat39 <yasinarafat2557@gmail.com>
2022-12-28 07:33:58 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="max-w-[1440px] mx-auto">
+      <p className='font-bold bg-slate-400'>paragraph</p>
+    </div>
+  );
+}
+
+export default App;
